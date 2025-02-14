54d4cb3dbef7395e71f8992f584f24a369a42a9f
Krishna Jeena <91063845+QAEYE@users.noreply.github.com>
2025-02-14 07:33:09 +0530

    basic commit

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, jsonify
+from youtube_transcript_api import YouTubeTranscriptApi
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/transcript/<video_id>', methods=['GET'])
+def get_transcript(video_id):
+    try:
+        transcript_data = YouTubeTranscriptApi.get_transcript(video_id)
+        transcript_text = "\n".join(entry["text"] for entry in transcript_data)
+        return jsonify({"transcript": transcript_text})
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
